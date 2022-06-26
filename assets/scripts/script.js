77c132a3e2457fd9c20a9268990ed3ae94903b93
Garrett Lockhart <lockhartphotography1@gmail.com>
2022-06-26 09:46:26 -0600

    added prompt functionality for user choices

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -6,16 +6,47 @@ var upperCaseList = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O'
 // prettier-ignore
 var lowerCaseList = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
-// Assignment Code
 var generateBtn = document.querySelector('#generate');
+// Add event listener to generate button
+generateBtn.addEventListener('click', writePassword);
 
 // Write password to the #password input
 function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector('#password');
+  var userSelectedPrompt = selectPrompts();
 
-  passwordText.value = password;
+  if (userSelectedPrompt) {
+    var password = generatePassword();
+    var passwordText = document.querySelector('#password');
+
+    passwordText.value = password;
+  }
 }
 
-// Add event listener to generate button
-generateBtn.addEventListener('click', writePassword);
+function selectPrompts() {
+  charChoices = [];
+
+  passwordLength = prompt('Please enter the desired length of your password between 8 - 128');
+  if (passwordLength < 8 || passwordLength > 128) {
+    alert('Please try again, must be between 8 - 128');
+    return false;
+  }
+
+  if (confirm('Do you want numbers in your password?')) {
+    charChoices = charChoices.concat(numberList);
+  }
+
+  if (confirm('Do you want symbols in your password?')) {
+    charChoices = charChoices.concat(symbolList);
+  }
+
+  if (confirm('Do you want upper case letters in your password?')) {
+    charChoices = charChoices.concat(upperCaseList);
+  }
+
+  if (confirm('Do you want lowercase letters in your password?')) {
+    charChoices = charChoices.concat(lowerCaseList);
+  }
+  return false;
+}
+
+function generatePassword() {}
