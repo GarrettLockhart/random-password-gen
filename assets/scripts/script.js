480611289c1cb8749446890075bec14759499c71
Garrett Lockhart <lockhartphotography1@gmail.com>
2022-06-28 00:37:43 -0600

    feat: logic is fully working

--- assets/scripts/script.js
+++ assets/scripts/script.js
@@ -1,6 +1,6 @@
 var passwordLength = 8;
 var charChoices = [];
-var numberList = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+var numberList = ['1', '2', '3', '4', '5', '6', '7', '8', '9'];
 var symbolList = ['!', '@', '#', '%', '^', '&', '*', '(', ')'];
 // prettier-ignore
 var upperCaseList = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z'];
@@ -8,17 +8,14 @@ var upperCaseList = ['A','B','C','D','E','F','G','H','I','J','K','L','M','N','O'
 var lowerCaseList = ['a','b','c','d','e','f','g','h','i','j','k','l','m','n','o','p','q','r','s','t','u','v','w','x','y','z'];
 
 var generateBtn = document.querySelector('#generate');
-// Add event listener to generate button
 generateBtn.addEventListener('click', writePassword);
 
-// Write password to the #password input
 function writePassword() {
   var userSelectedPrompt = selectPrompts();
 
   if (userSelectedPrompt) {
     randomPassword = generatePassword();
     var passwordText = document.querySelector('#password');
-
     passwordText.value = randomPassword;
   }
 }
@@ -29,7 +26,7 @@ function selectPrompts() {
   passwordLength = parseInt(
     prompt('Please enter the desired length of your password between 8 - 128')
   );
-  if (passwordLength < 8 || passwordLength > 128) {
+  if (passwordLength < 8 || passwordLength > 128 || isNaN(passwordLength)) {
     alert('Please try again, must be between 8 - 128');
     return false;
   }
@@ -49,14 +46,14 @@ function selectPrompts() {
   if (confirm('Do you want lowercase letters in your password?')) {
     charChoices = charChoices.concat(lowerCaseList);
   }
-  return false;
+  return charChoices;
 }
 
 function generatePassword() {
-  randomPassword = '';
+  var randomPassword = '';
   for (let i = 0; i < passwordLength; i++) {
-    var randomChars = charChoices[Math.floor(Math.random() * charChoices.length)];
+    var randomArrayIndex = Math.floor(Math.random() * charChoices.length);
+    randomPassword += charChoices[randomArrayIndex];
   }
-  console.log(randomChars);
-  return randomChars;
+  return randomPassword;
 }
